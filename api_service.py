074cefc15c8bfeabae1404639d32d7df55cf54ce
Polina Rasskazova <rasskazova.pm@phystech.edu>
2022-01-22 08:53:08 +0300

    refactor: account_id

--- api_service.py
+++ api_service.py
@@ -4,21 +4,21 @@ import requests
 headers = {'User-Agent': 'huntflow-test-app (incaseoffire@example.com)'}
 
 api_url = 'https://dev-100-api.huntflow.dev'
-
+account_id = 2
 
 def set_auth(token):
     headers['Authorization'] = f'Bearer {token}'
 
 
 def post_applicants(applicant):
-    return requests.post(f'{api_url}/account/2/applicants', data=json.dumps(applicant), headers=headers)
+    return requests.post(f'{api_url}/account/{account_id}/applicants', data=json.dumps(applicant), headers=headers)
 
 
 def get_vacancies():
-    r = requests.get(f'{api_url}/account/2/vacancies', headers=headers).json()
+    r = requests.get(f'{api_url}/account/{account_id}/vacancies', headers=headers).json()
     return r['items']
 
 
 def get_statuses():
-    r = requests.get(f'{api_url}/account/2/vacancy/statuses', headers=headers).json()
+    r = requests.get(f'{api_url}/account/{account_id}/vacancy/statuses', headers=headers).json()
     return r['items']
